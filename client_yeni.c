f650cceb62755b519bf8f847934ec28c2f2c9b76
dogacsoylu <148787660+dogacsoylu@users.noreply.github.com>
2024-10-13 15:07:58 +0300

    Update client_yeni.c

--- client_yeni.c
+++ client_yeni.c
@@ -80,7 +80,7 @@ void* buffer;
 while(1){
 
    scanf("Mesaj göndermek için 1'e bas sonra mesajı gönder: %s", mesaj);
-   if(mesaj == "1"){
+   if(strcmp(mesaj,"1") == 0){
       scanf("Yaz: %s", mesaj);
       giden_bytelar = send(soketfd, mesaj, giden_uzunluk, 0);
    }
